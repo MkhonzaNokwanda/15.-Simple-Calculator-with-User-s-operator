5fe023db6ada8a47354241c4c02ec30b4020b5c1
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-13 00:05:59 +0200

    Add project files.

---
+++ 15. Simple Calculator with User's operator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Simple_Calculator_with_User_s_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //call for a method
+            Calculator();
+
+            //freeze the console
+            Console.ReadLine();
+        }
+        static void Calculator()
+        {
+            Console.WriteLine("Enter first number");
+            double x  = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Choose your operator");
+            string op = Console.ReadLine();
+            Console.WriteLine("Enter second number");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double Answer;
+            if (op == "+")
+            {
+                Console.WriteLine(" The sum is ");
+                Console.WriteLine(Answer = x + y);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(" The difference is ");
+                Console.WriteLine(Answer = x - y);
+            }
+            else if(op == "*")
+            {
+                Console.WriteLine(" The product is ");
+                Console.WriteLine(Answer = x * y);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(" The quotient is ");
+                Console.WriteLine(Answer = x / y);
+            }
+            else
+            {
+                Console.WriteLine(" Not an operator");
+            }
+        }
+    }
+}
